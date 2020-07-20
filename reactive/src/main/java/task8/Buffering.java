0fa3b8cc9d9c8aa425e125dd40f219243dbfe7f8
Kirill Savchuk <kirillss1998@gmail.com>
2020-07-20 21:00:05 +0300

    Completed task8

---
+++ reactive/src/main/java/task8/Buffering.java
@@ -0,0 +1,31 @@
+package task8;
+
+import io.reactivex.rxjava3.annotations.NonNull;
+import io.reactivex.rxjava3.core.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+public class Buffering {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Observable.range(1, 30)
+                .buffer(4, 7)
+                .subscribe(System.out::println);
+
+//        Observable.interval(500, TimeUnit.MILLISECONDS)
+//                .buffer(1, TimeUnit.SECONDS, 2)
+//                .subscribe(System.out::println);
+
+        @NonNull
+        Observable<Long> interval = Observable.interval(500, TimeUnit.MILLISECONDS);
+
+        Observable.interval(1000, TimeUnit.MILLISECONDS)
+                .window(interval)
+                .subscribe(System.out::println);
+
+        Thread.sleep(10000);
+
+    }
+
+}
